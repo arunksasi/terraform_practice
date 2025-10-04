bdcf4941cfb93a8f6a65887b4fb9ff5024758226
Arun K Sasi <arunksasi33@gmail.com>
2025-10-04 12:18:49 +0530

    Main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
-data "aws_ami" "ubuntu"{
+data "aws_ami" "ubuntu" {
+  most_recent = true
 
-    most_recent = true 
-    filter {
-      name = "name"
-      values = ["ubuntu/images/hvm-ssd/"]
+  owners = ["099720109477"] # Canonical's official AWS account ID
 
-    }
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
 
-    filter {
-      name ="virtualization type "
-      values = ["hvm"]
-    }
-
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 }
