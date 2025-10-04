1bc59b1c3ddbccb418458f5c5ce641f8988e5a63
Arun K Sasi <arunksasi33@gmail.com>
2025-10-04 16:50:42 +0530

    added outputs

--- output.tf
+++ output.tf
@@ -2,3 +2,24 @@ output "ami_id" {
   value = data.aws_ami.ubuntu.id
 
 }
+
+output "instance_name" {
+
+    value = var.instance_name
+  
+}
+
+output "subnet_id - public" {
+    
+   
+
+    value = [for subnet in  module.vpc.aws.subnet.public : subnet.id  ]
+  
+}
+
+
+output "subnet_id -private" {
+
+    value =  [for subnet in module.vpc.aws.subnet.private : subnet.id]
+  
+}
