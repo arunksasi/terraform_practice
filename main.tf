06dd5c6adb9fa41ba008f13499c225df3dd3d580
Arun K Sasi <arunksasi33@gmail.com>
2025-10-04 12:33:28 +0530

    Main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,12 @@
 data "aws_ami" "ubuntu"{
 
     most_recent = true 
+    owners = ["099720109477"] # Canonical's official AWS account ID
     filter {
       name = "name"
-      values = ["ubuntu/images/hvm-ssd/"]
+      values = ["ubuntu/images/hvm-ssd/"] 
 
+     
     }
 
     filter {
